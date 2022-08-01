b8f1f574ee0d3230cab47d20d3b50dfb6b09a8f3
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-08-01 18:01:32 +0200

    first setup

---
+++ HotelRentalApplication.Backend/HotelRentalManager.Shared/Common/BaseObject.cs
@@ -0,0 +1,6 @@
+﻿namespace HotelRentalManager.Shared.Common;
+
+public abstract class BaseObject
+{
+    public Guid Id { get; set; }
+}
---
+++ HotelRentalApplication.Backend/HotelRentalManager.Shared/Common/BaseRequestObject.cs
@@ -0,0 +1,6 @@
+﻿namespace HotelRentalManager.Shared.Common;
+
+public abstract class BaseRequestObject
+{
+    public Guid? Id { get; set; }
+}
