c3beec16dc500aa8f0e9ef3f4b2c5a50c383436e
NicoSilve22 <ns.nicolasilvestri@libero.it>
2022-08-02 12:45:18 +0200

    completed authentication

---
+++ HotelRentalApplication.Backend/HotelRentalManager/Controllers/MeController.cs
@@ -0,0 +1,33 @@
+﻿using HotelRentalManager.Abstractions.Controllers;
+using HotelRentalManager.Authentication.Extensions;
+using HotelRentalManager.Shared.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelRentalManager.Controllers;
+
+public class MeController : ApiController
+{
+    [HttpGet("GetMe")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    public IActionResult GetMe()
+    {
+        var user = new User
+        {
+            Id = User.GetId(),
+            FirstName = User.GetFirstName(),
+            LastName = User.GetLastName(),
+            DateOfBirth = User.GetDateOfBirth(),
+            Age = User.GetAge(),
+            Gender = User.GetGender(),
+            City = User.GetCity(),
+            Country = User.GetCountry(),
+            PhoneNumber = User.GetPhoneNumber(),
+            Email = User.GetEmail(),
+            UserName = User.GetUserName()
+        };
+
+        return Ok(user);
+    }
+}
